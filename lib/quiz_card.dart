275f84bb011019ae842c4165f2a9afff1225f4f6
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-06-15 11:13:31 +0530

    Removed Templates folder Made them a Seperate Repository.

--- lib/quiz_card.dart
+++ lib/quiz_card.dart
@@ -27,86 +27,85 @@ class _QuizCardState extends State<QuizCard> {
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 20),
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: MediaQuery.of(context).size.height * 0.56,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.56,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        elevation: 10,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Card(
+                            color:
+                                _selectedButton ? Colors.white : Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            elevation: 10,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(15.0),
+                              onTap: () {
+                                setState(() {
+                                  _selectedButton = false;
+                                });
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.1,
+                                child: Center(
+                                  child: Text(
+                                    'False',
+                                    style:
+                                        TextStyle(fontFamily: '', fontSize: 40),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                          elevation: 10,
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: MediaQuery.of(context).size.height * 0.2,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Card(
-                              color:
-                                  _selectedButton ? Colors.white : Colors.green,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              elevation: 10,
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(15.0),
-                                onTap: () {
-                                  setState(() {
-                                    _selectedButton = false;
-                                  });
-                                },
-                                child: Container(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.35,
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.1,
-                                  child: Center(
-                                    child: Text(
-                                      'False',
-                                      style: TextStyle(
-                                          fontFamily: '', fontSize: 40),
-                                    ),
+                          Card(
+                            color:
+                                _selectedButton ? Colors.green : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            elevation: 10,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(15.0),
+                              onTap: () {
+                                setState(() {
+                                  _selectedButton = true;
+                                });
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.1,
+                                child: Center(
+                                  child: Text(
+                                    'True',
+                                    style:
+                                        TextStyle(fontFamily: '', fontSize: 40),
                                   ),
                                 ),
                               ),
                             ),
-                            Card(
-                              color:
-                                  _selectedButton ? Colors.green : Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              elevation: 10,
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(15.0),
-                                onTap: () {
-                                  setState(() {
-                                    _selectedButton = true;
-                                  });
-                                },
-                                child: Container(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.35,
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.1,
-                                  child: Center(
-                                    child: Text(
-                                      'True',
-                                      style: TextStyle(
-                                          fontFamily: '', fontSize: 40),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
